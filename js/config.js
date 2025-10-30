39bbfdbe8a81dc38db95961ec6b345c57b690dc3
FunkyPhil94 <122721265+FunkyPhil94@users.noreply.github.com>
2025-10-30 16:41:59 +0100

    Update CSV_URL to use CSV output format

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // >>>>>>>>>>>> HIER DEINE VERÖFFENTLICHTE SHEETS-CSV-URL EINTRAGEN <<<<<<<<<<<<
-export const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRg81HZFdz1UqGpGP2hbobxDSY4Pqib25LcxppShhYuVU9DHxWYb7QV-qpjZpnXvyOuU15_t1HeKPGk/pubhtml?gid=522258962&single=true';
+export const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRg81HZFdz1UqGpGP2hbobxDSY4Pqib25LcxppShhYuVU9DHxWYb7QV-qpjZpnXvyOuU15_t1HeKPGk/pub?gid=522258962&single=true&output=csv';
 // Auto-Reload in Minuten (0 = aus)
 export const REFRESH_MINUTES = 0.5;
